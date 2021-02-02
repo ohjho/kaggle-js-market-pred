68c428bc10b584004de5af2041e6840745d84b9a
ohjho <jho.yvr+git@gmail.com>
2021-02-02 19:28:52 +0800

    dropna in train_df

--- data_prep.py
+++ data_prep.py
@@ -23,7 +23,7 @@ def df_64bits_to_32bits(df, verbose = False):
         print(f'df memory use after conversion: {df.memory_usage(index=True, deep = True).sum()}')
     return df
 
-def get_train_df(filepath, min_weight = None, min_resp = 0):
+def get_train_df(filepath, min_weight = None, min_resp = 0, dropna = True):
     s_time = time.time()
     train_dt = dt.fread(filepath)
     train_df = df_64bits_to_32bits(train_dt.to_pandas(), verbose= True)
@@ -31,6 +31,10 @@ def get_train_df(filepath, min_weight = None, min_resp = 0):
     r_time = round(time.time() - s_time, 2)
     print(f'df took {r_time} seconds to load')
 
+    if dropna:
+        len_with_na = len(train_df)
+        train_df = train_df.dropna(axis = 0)
+        print(f'{len_with_na - len(train_df)} rows with NA values dropped.')
     if min_weight and 'weight' in train_df.columns:
         print(f'{len(train_df)} trades in raw trainset')
         train_df = train_df[train_df['weight']> min_weight]
@@ -39,7 +43,7 @@ def get_train_df(filepath, min_weight = None, min_resp = 0):
     # Target Definition
     if 'resp' in train_df.columns:
         train_df['action'] = (train_df['resp']> min_resp).astype('int')
-        print(f'actionable trades pct: {round(train_df["action"].sum()/len(train_df),2)}')
+        print(f'actionable trades pct: {round((train_df["action"].sum()/len(train_df))*100,2)}')
     return train_df
 
 def ts_df_split(df, validate_pct = 0.3, verbose = False):
