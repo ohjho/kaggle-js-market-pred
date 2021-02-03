2c2d15672e6c44eae3e950974d090f30e44dc7df
ohjho <jho.yvr+git@gmail.com>
2021-02-03 11:59:33 +0800

    added new function to reduce_memory_usage

--- data_prep.py
+++ data_prep.py
@@ -23,10 +23,47 @@ def df_64bits_to_32bits(df, verbose = False):
         print(f'df memory use after conversion: {df.memory_usage(index=True, deep = True).sum()}')
     return df
 
+def reduce_memory_usage(df):
+    start_memory = df.memory_usage().sum() / 1024**2
+    print(f"Memory usage of dataframe is {start_memory} MB")
+
+    for col in df.columns:
+        col_type = df[col].dtype
+
+        if col_type != 'object':
+            c_min = df[col].min()
+            c_max = df[col].max()
+
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    pass
+        else:
+            df[col] = df[col].astype('category')
+
+    end_memory = df.memory_usage().sum() / 1024**2
+    print(f"Memory usage of dataframe after reduction {end_memory} MB")
+    print(f"Reduced by {100 * (start_memory - end_memory) / start_memory} % ")
+    return df
+
 def get_train_df(filepath, min_weight = None, min_resp = 0, dropna = True):
     s_time = time.time()
     train_dt = dt.fread(filepath)
-    train_df = df_64bits_to_32bits(train_dt.to_pandas(), verbose= True)
+    #train_df = df_64bits_to_32bits(train_dt.to_pandas(), verbose= True)
+    train_df = reduce_memory_usage(train_dt.to_pandas())
     train_df.set_index('ts_id')
     r_time = round(time.time() - s_time, 2)
     print(f'df took {r_time} seconds to load')
@@ -34,7 +71,7 @@ def get_train_df(filepath, min_weight = None, min_resp = 0, dropna = True):
     if dropna:
         len_with_na = len(train_df)
         train_df = train_df.dropna(axis = 0)
-        print(f'{round(len_with_na / len(train_df),2)*100}% of rows with NA values dropped.')
+        print(f'{round((len(train_df)/len_with_na)-1,2)*100}% of rows with NA values dropped.')
 
     if min_weight and 'weight' in train_df.columns:
         print(f'{len(train_df)} trades in raw trainset')
